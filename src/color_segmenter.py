1c008e7e30a4c40b961bb423a2118450f6dc11ff
Artur <arturalmeida@ua.pt>
2024-10-20 22:51:02 +0100

    Cleaned some stuff

--- src/color_segmenter.py
+++ src/color_segmenter.py
@@ -1,6 +1,6 @@
 import cv2
 from functools import partial
-from readchar import key
+import json
 
 def save(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -17,23 +17,29 @@ def main():
     limits = {"B": {"max": 255, "min": 0},"G": {"max": 255, "min": 0},"R": {"max": 255, "min": 0}}
     ret, frame = capture.read() 
     #cv2.imshow(window_name,frame)
-    cv2.createTrackbar("B-min",window_name,limits["B"]["min"],255,partial(onTrackbar, mm="min", C="B",limits=limits))
-    cv2.createTrackbar("B-max",window_name,limits["B"]["max"],255,partial(onTrackbar, mm="max", C="B",limits=limits))
-    cv2.createTrackbar("G-min",window_name,limits["G"]["min"],255,partial(onTrackbar, mm="min", C="G",limits=limits))
-    cv2.createTrackbar("G-max",window_name,limits["G"]["max"],255,partial(onTrackbar, mm="max", C="G",limits=limits))
-    cv2.createTrackbar("R-min",window_name,limits["R"]["min"],255,partial(onTrackbar, mm="min", C="R",limits=limits))
-    cv2.createTrackbar("R-max",window_name,limits["R"]["max"],255,partial(onTrackbar, mm="max", C="R",limits=limits))
+    cv2.createTrackbar("B-min",window_name,limits["B"]["min"],255,lambda threshold: onTrackbar(threshold, mm="min", C="B",limits=limits))
+    cv2.createTrackbar("B-max",window_name,limits["B"]["max"],255,lambda threshold: onTrackbar(threshold, mm="max", C="B",limits=limits))
+    cv2.createTrackbar("G-min",window_name,limits["G"]["min"],255,lambda threshold: onTrackbar(threshold, mm="min", C="G",limits=limits))
+    cv2.createTrackbar("G-max",window_name,limits["G"]["max"],255,lambda threshold: onTrackbar(threshold, mm="max", C="G",limits=limits))
+    cv2.createTrackbar("R-min",window_name,limits["R"]["min"],255,lambda threshold: onTrackbar(threshold, mm="min", C="R",limits=limits))
+    cv2.createTrackbar("R-max",window_name,limits["R"]["max"],255,lambda threshold: onTrackbar(threshold, mm="max", C="R",limits=limits))
 
     
     while (True):
-        ret, frame = capture.read()   
-        mask = cv2.inRange(frame,(limits["B"]["min"],limits["G"]["min"],limits["R"]["min"]),(limits["B"]["max"],limits["G"]["max"],limits["R"]["max"]))       
+        ret, frame = capture.read() 
 
+        mask = cv2.inRange(frame,(limits["B"]["min"],limits["G"]["min"],limits["R"]["min"]),(limits["B"]["max"],limits["G"]["max"],limits["R"]["max"]))       
         cv2.imshow(window_name,mask)
 
-        # add code to create the trackbar ...
-        if cv2.waitKey(1) == ord("q"):
+        k = cv2.waitKey(1)
+
+        if k == ord("q"):
             break
+        elif k == ord("w"):
+            file_name = 'limits.json'
+            with open(file_name, 'w') as json_file:
+                print('writing dictionary d to file ' + file_name)
+                json.dump({"limits": limits}, json_file, indent=4)
 
 if __name__ == '__main__':
     main()
